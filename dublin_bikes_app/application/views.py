c0c25ec1ccafdf712f9d3799c265be96eb661ab0
crotty-d <darragh@darraghcrotty.com>
2018-04-04 20:23:57 +0100

    updated chart data route in flask

--- dublin_bikes_app/application/views.py
+++ dublin_bikes_app/application/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, jsonify
+from flask import render_template, jsonify, g
 from application import app
 from application import db
 
@@ -6,20 +6,47 @@ from application import db
 def index():
 	return render_template("index.html")
 
-@app.route('/daily_bikes_available/<int:station_num>')
-def daily_bikes_json(station_num):
-	"""REST API response providing json file with average bike availability for each day of the week"""
+
+@app.route('/station_stats/<int:station_num>')
+def get_chart_data(station_num):
+	"""REST API response providing json file with data for charts"""
 	
-	sql = """SELECT avg(bikes_available) AS bikes
+	# MySQL query to get average daily availability for a given station
+	sql_daily = """SELECT ROUND(AVG(bikes_available)) AS bikes
 	FROM bikesdata.stations
 	WHERE station_number = {}
 	GROUP BY day(update_time);""".format(station_num)
 	
-	# Execute SQL query
-	result = db.engine.execute(sql)
+	# MySQL query to get average daily availability for a given station
+	sql_hourly = """replace this with SQL query for specified station number {};""".format(station_num) #TODO: Add query
 	
+	# TODO: SQL query for weather-related availability chart
+	
+	# Execute SQL query for daily averages
+	daily_data = db.engine.execute(sql_daily)
+	
+	# Execute SQL query for hourly averages
+	#hourly_data = db.engine.execute(sql_hourly) TODO: Uncomment when SQL query completed
+	
+	# TODO: Execute weather-related chart SQL query
+	
+	# Get data from queries and structure as JSON file (list of dictionaries)
 	data = []
-	for row in result:
+	# Daily
+	for row in daily_data:
 		data.append(dict(row))
+	# Hourly
+		# TODO: Add code to append hourly data to data variable (list)
+		# Note: Try and create relatively neat and readable JSON file if possible
+	# Weather
+		# TODO: Add code to append weather related availability data to data variable (list)
 	
-	return jsonify(daily_bikes_available=data)
+	# Return JSON file as HTTP response
+	return jsonify(station_stats=data)
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+	db = getattr(g, '_database', None)
+	if db is not None:
+		db.close()
