53b3d0f1d23a3ab1c346ef3ea323b97d4f1e58ca
crotty-d <darragh@darraghcrotty.com>
2018-04-04 21:12:38 +0100

    Nicer JSON output from get_chart_data function in views.py

--- dublin_bikes_app/application/views.py
+++ dublin_bikes_app/application/views.py
@@ -12,10 +12,10 @@ def get_chart_data(station_num):
 	"""REST API response providing json file with data for charts"""
 	
 	# MySQL query to get average daily availability for a given station
-	sql_daily = """SELECT ROUND(AVG(bikes_available)) AS bikes
+	sql_daily = """SELECT dayname(update_time) AS 'day', ROUND(AVG(bikes_available)) AS 'bikes'
 	FROM bikesdata.stations
 	WHERE station_number = {}
-	GROUP BY day(update_time);""".format(station_num)
+	GROUP BY dayname(update_time);""".format(station_num)
 	
 	# MySQL query to get average daily availability for a given station
 	sql_hourly = """replace this with SQL query for specified station number {};""".format(station_num) #TODO: Add query
@@ -23,23 +23,28 @@ def get_chart_data(station_num):
 	# TODO: SQL query for weather-related availability chart
 	
 	# Execute SQL query for daily averages
-	daily_data = db.engine.execute(sql_daily)
+	daily_result = db.engine.execute(sql_daily) # result is a RowProxy
 	
 	# Execute SQL query for hourly averages
-	#hourly_data = db.engine.execute(sql_hourly) TODO: Uncomment when SQL query completed
+	#hourly_result = db.engine.execute(sql_hourly) TODO: Uncomment when SQL query completed
 	
 	# TODO: Execute weather-related chart SQL query
 	
 	# Get data from queries and structure as JSON file (list of dictionaries)
-	data = []
+	data = {'daily_avg': None, 'hourly_avg': None, 'weather': None}
+	daily_data = {}
+	
 	# Daily
-	for row in daily_data:
-		data.append(dict(row))
+	for row in daily_result:
+		daily_data[row['day']] = row['bikes'] # Note each row in RowProxy is dictionary: {col_name: col_value_for_row}
+		
+	data['daily_avg'] = daily_data
+	
 	# Hourly
-		# TODO: Add code to append hourly data to data variable (list)
+		# TODO: Add code to put hourly into data['hourly_avg']
 		# Note: Try and create relatively neat and readable JSON file if possible
 	# Weather
-		# TODO: Add code to append weather related availability data to data variable (list)
+		# TODO: Add code to put weather related availability data into data['weather']
 	
 	# Return JSON file as HTTP response
 	return jsonify(station_stats=data)
