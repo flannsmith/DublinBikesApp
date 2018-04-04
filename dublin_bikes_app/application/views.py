fdd563186d25339ca990995b2f68d7cbfd080e85
crotty-d <darragh@darraghcrotty.com>
2018-04-04 19:05:51 +0100

    get daily averages route added to view.py

--- dublin_bikes_app/application/views.py
+++ dublin_bikes_app/application/views.py
@@ -1,16 +1,25 @@
-from flask import render_template
+from flask import render_template, jsonify
 from application import app
 from application import db
 
 @app.route('/')
 def index():
-	sql = 'SELECT * FROM bikesdata.stations WHERE station_number = 55;'
+	return render_template("index.html")
+
+@app.route('/daily_bikes_available/<int:station_num>')
+def daily_bikes_json(station_num):
+	"""REST API response providing json file with average bike availability for each day of the week"""
+	
+	sql = """SELECT avg(bikes_available) AS bikes
+	FROM bikesdata.stations
+	WHERE station_number = {}
+	GROUP BY day(update_time);""".format(station_num)
+	
+	# Execute SQL query
 	result = db.engine.execute(sql)
 	
 	data = []
 	for row in result:
-		data.append(row)
-		print("Address:", row['address'])
+		data.append(dict(row))
 	
-	returnDict = {'station': data[0]['address'], 'available': data[0]['bikes_available'], 'slots': data[0]['stands_available']}
-	return render_template("index.html", **returnDict)
+	return jsonify(daily_bikes_available=data)
